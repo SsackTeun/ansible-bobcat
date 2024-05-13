2f70db35dac577420fcf40e0bd025edab469e9e3
root <root@localhost.localdomain>
2024-05-13 15:43:45 +0900

    레지스트리 push success

--- 00_prepare/02_registry/run.sh
+++ 00_prepare/02_registry/run.sh
@@ -2,4 +2,6 @@
 # 실행
 
 ansible-playbook yaml/prepare_create_registry.yml
-ansible-playbook yaml/prepare_push_image_to_myregistry.yml 
+ansible-playbook yaml/prepare_push_image_to_myregistry.yml
+curl -u admin:admin http://myregistry:5000/v2/_catalog
+ 
