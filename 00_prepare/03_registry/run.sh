560777e8b2fd8cfac7de2470eef116ab507471a3
root <root@localhost.localdomain>
2024-05-17 17:33:13 +0900

    04_repository 동작 정상 확인

--- 00_prepare/03_registry/run.sh
+++ 00_prepare/03_registry/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # 실행
 
-#ansible-playbook yaml/prepare_create_registry.yml
+ansible-playbook yaml/prepare_create_registry.yml
 ansible-playbook yaml/prepare_load_image.yml
 ansible-playbook yaml/prepare_push_image_to_myregistry.yml
-#curl -u admin:admin http://myregistry:5000/v2/_catalog
+curl -u admin:admin http://myregistry:5000/v2/_catalog
  
